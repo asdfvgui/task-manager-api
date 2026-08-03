9d926651464d150ca4b5d8f2400864c967496f1d
gitguiandrade <asdfvgui@gmail.com>
2026-08-03 17:48:26 -0500

    feat: add response dtos

---
+++ src/main/java/com/asdfvgui/taskmanagerapi/dto/response/TaskResponse.java
@@ -0,0 +1,38 @@
+package com.asdfvgui.taskmanagerapi.dto.response;
+
+
+public class TaskResponse {
+
+
+    private Long id;
+    private String title;
+    private String description;
+    private boolean completed;
+
+
+    public TaskResponse(Long id, String title, String description, boolean completed) {
+
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.completed = completed;
+
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+}
