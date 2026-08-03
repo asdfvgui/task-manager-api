3bffd06dee638026e9f859d0457895d02226c6b5
gitguiandrade <asdfvgui@gmail.com>
2026-08-03 16:37:56 -0500

    feat: create task request dto

---
+++ src/main/java/com/asdfvgui/taskmanagerapi/dto/request/CreateTaskRequest.java
@@ -0,0 +1,27 @@
+package com.asdfvgui.taskmanagerapi.dto.request;
+
+
+public class CreateTaskRequest {
+
+
+    private String title;
+    private String description;
+
+
+    public CreateTaskRequest() {}
+
+
+    public String getTitle() {
+        return title;
+    }
+    public String getDescription() {
+        return description;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
