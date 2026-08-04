e5fc04a0d5b9ec718b5c19757b4efbd2e39bd2bc
gitguiandrade <asdfvgui@gmail.com>
2026-08-04 02:11:27 -0500

    feat: add get all tasks endpoint

--- src/main/java/com/asdfvgui/taskmanagerapi/controller/TaskController.java
+++ src/main/java/com/asdfvgui/taskmanagerapi/controller/TaskController.java
@@ -5,10 +5,9 @@ import com.asdfvgui.taskmanagerapi.dto.request.CreateTaskRequest;
 import com.asdfvgui.taskmanagerapi.dto.response.TaskResponse;
 import com.asdfvgui.taskmanagerapi.model.entity.Task;
 import com.asdfvgui.taskmanagerapi.service.TaskService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @RestController
@@ -29,5 +28,22 @@ public class TaskController {
     public TaskResponse createTask(@RequestBody CreateTaskRequest request) {
         Task task = taskService.createTask(request.getTitle(), request.getDescription());
         return new TaskResponse(task.getId(), task.getTitle(), task.getDescription(), task.isCompleted());
+    }
+
+    @GetMapping
+    public List<TaskResponse> getAllTasks() {
+
+        List<Task> tasks = taskService.getAllTasks();
+
+        List<TaskResponse> taskResponses = new ArrayList<>();
+
+        for (Task task : tasks){
+
+            taskResponses.add(new TaskResponse(task.getId(), task.getTitle(), task.getDescription(), task.isCompleted()));
+
+        }
+
+        return taskResponses;
+
     }
 }
