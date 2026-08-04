4f08ad68cd3bc26afb39973314b81c1ae5966816
gitguiandrade <asdfvgui@gmail.com>
2026-08-04 00:44:04 -0500

    Implement create task endpoint

--- src/main/java/com/asdfvgui/taskmanagerapi/controller/TaskController.java
+++ src/main/java/com/asdfvgui/taskmanagerapi/controller/TaskController.java
@@ -1,12 +1,20 @@
 package com.asdfvgui.taskmanagerapi.controller;
 
+
+import com.asdfvgui.taskmanagerapi.dto.request.CreateTaskRequest;
+import com.asdfvgui.taskmanagerapi.dto.response.TaskResponse;
+import com.asdfvgui.taskmanagerapi.model.entity.Task;
 import com.asdfvgui.taskmanagerapi.service.TaskService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @RequestMapping("/tasks")
 public class TaskController {
+
 
     private final TaskService taskService;
 
@@ -14,5 +22,12 @@ public class TaskController {
 
         this.taskService = taskService;
 
+    }
+
+
+    @PostMapping
+    public TaskResponse createTask(@RequestBody CreateTaskRequest request) {
+        Task task = taskService.createTask(request.getTitle(), request.getDescription());
+        return new TaskResponse(task.getId(), task.getTitle(), task.getDescription(), task.isCompleted());
     }
 }
